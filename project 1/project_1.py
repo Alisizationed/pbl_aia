0ce1610e07482982590c8f3b05ea12aadd91e6cd
Linage <p.anastasia.danu@gmail.com>
2026-02-03 21:16:25 +0200

    chore: add validation to the stations

--- project 1/project_1.py
+++ project 1/project_1.py
@@ -1,139 +1,177 @@
-import math  
+import math
+import numbers
 
-def calculeaza_distanta_euclidiana(p1, p2): 
-  """Distanța în metri (presupunem 1 unitate = 1 metru)"""
-  dx = p2['x'] - p1['x']
-  dy = p2['y'] - p1['y'] 
 
-  return math.sqrt(dx**2 + dy**2)  
-  
+def calculeaza_distanta_euclidiana(p1, p2):
+    """Distanța în metri (presupunem 1 unitate = 1 metru)"""
+    dx = p2['x'] - p1['x']
+    dy = p2['y'] - p1['y']
+
+    return math.sqrt(dx ** 2 + dy ** 2)
+
+
 def calculeaza_timp_mers_pe_jos(distanta_metri):
-  """ Calculează timp mers pe jos Viteza medie: 5 km/h = 83.33 m/min"""
-  viteza_m_pe_min = 83.33
-  timp_minute = distanta_metri / viteza_m_pe_min 
+    """ Calculează timp mers pe jos Viteza medie: 5 km/h = 83.33 m/min"""
+    viteza_m_pe_min = 83.33
+    timp_minute = distanta_metri / viteza_m_pe_min
 
-  return timp_minute
+    return timp_minute
+
 
 def analizeaza_statie(statie, pozitie_student):
-  """ Analizează o stație și returnează timpul total minim """
-  # Distanța până la stație
-  distanta_la_statie = calculeaza_distanta_euclidiana(pozitie_student,  statie)
-  timp_mers = calculeaza_timp_mers_pe_jos(distanta_la_statie) 
+    """ Analizează o stație și returnează timpul total minim """
+    # Distanța până la stație
+    distanta_la_statie = calculeaza_distanta_euclidiana(pozitie_student, statie)
+    timp_mers = calculeaza_timp_mers_pe_jos(distanta_la_statie)
 
-  # Analizăm fiecare linie de autobuz
-  optiuni_linii = [] 
+    # Analizăm fiecare linie de autobuz
+    optiuni_linii = []
 
-  for linie in statie['linii']: 
+    for linie in statie['linii']:
+        timp_asteptare_mediu = linie['frecventa_min'] / 2
+        timp_calatorie = linie['durata_calatorie_min']
+        timp_total = timp_mers + timp_asteptare_mediu + timp_calatorie
 
-    timp_asteptare_mediu = linie['frecventa_min'] / 2 
-    timp_calatorie = linie['durata_calatorie_min'] 
-    timp_total = timp_mers + timp_asteptare_mediu + timp_calatorie 
-
-    optiuni_linii.append({
-        'linie': linie['numar'], 
-        'timp_mers': timp_mers,
-        'timp_asteptare': timp_asteptare_mediu,
-        'timp_calatorie': timp_calatorie, 
-        'timp_total': timp_total
+        optiuni_linii.append({
+            'linie': linie['numar'],
+            'timp_mers': timp_mers,
+            'timp_asteptare': timp_asteptare_mediu,
+            'timp_calatorie': timp_calatorie,
+            'timp_total': timp_total
         })
 
-  # Găsim cea mai rapidă linie de la această stație 
-  cea_mai_rapida = min(optiuni_linii, key=lambda o: o['timp_total'])
-  
-  return {
-      'statie': statie['nume'], 
-      'distanta_la_statie_m': distanta_la_statie, 
-      'cea_mai_rapida_optiune': cea_mai_rapida, 
-      'toate_optiunile': optiuni_linii
-      }  
+    # Găsim cea mai rapidă linie de la această stație
+    cea_mai_rapida = min(optiuni_linii, key=lambda o: o['timp_total'])
 
-def gaseste_statie_optima(statii, pozitie_student): 
-  """ Analizează toate stațiile și găsește opțiunea optimă """
-  analize = [] 
+    return {
+        'statie': statie['nume'],
+        'distanta_la_statie_m': distanta_la_statie,
+        'cea_mai_rapida_optiune': cea_mai_rapida,
+        'toate_optiunile': optiuni_linii
+    }
 
-  for statie in statii:
-    analiza = analizeaza_statie(statie, pozitie_student)
-    analize.append(analiza)
 
-  # Sortăm după timp total
-  analize_sortate = sorted(analize, key=lambda a: a['cea_mai_rapida_optiune']['timp_total'])
+def is_number(value):
+    """ Verify if the value is a Number """
+    return isinstance(value, numbers.Number) and not isinstance(value, bool)
 
-  return analize_sortate  
 
-# Program principal 
-def main_autobuz(): 
-  
-  pozitie_student = {"x": 100, "y": 150}
-  statii = [{
-    "nume": "Stația Piață", 
-    "x": 120, 
-    "y": 180, 
-    "linii": 
-      [{
-          "numar": "21", 
-          "frecventa_min": 10,
-          "durata_calatorie_min": 25
-      }, 
-      {
-          "numar": "7",
-          "frecventa_min": 15,
-          "durata_calatorie_min": 22
-          }
-      ]
+def line_is_valid(line):
+    """ Analize whether the line is valid or not"""
+    if line['frecventa_min'] <= 0 or line['durata_calatorie_min'] <= 0:
+        return False
+
+    return True
+
+
+def station_is_valid(station):
+    """ Analize whether the station is valid or not """
+    # Verify if the coords are numeric
+    if not is_number(station['x']) or not is_number(station['y']):
+        print(f"{station['nume']} has wrong coordinates")
+        return False
+
+    # Skip invalid lines
+    station['linii'] = [line for line in station['linii'] if line_is_valid(line)]
+
+    # Verify if this station has lines
+    if len(station['linii']) == 0:
+        print(f"{station['nume']} doesn't have any lines")
+        return False
+
+    return True
+
+
+def gaseste_statie_optima(statii, pozitie_student):
+    """ Analizează toate stațiile și găsește opțiunea optimă= """
+    analize = []
+
+    for statie in statii:
+        # Skip invalid stations
+        if not station_is_valid(statie):
+            continue
+        analiza = analizeaza_statie(statie, pozitie_student)
+        analize.append(analiza)
+
+    # Sortăm după timp total
+    analize_sortate = sorted(analize, key=lambda a: a['cea_mai_rapida_optiune']['timp_total'])
+
+    return analize_sortate
+
+
+# Program principal
+def main_autobuz():
+    pozitie_student = {"x": 100, "y": 150}
+    statii = [{
+        "nume": "Stația Piață",
+        "x": 120,
+        "y": 180,
+        "linii":
+            [{
+                "numar": "21",
+                "frecventa_min": 10,
+                "durata_calatorie_min": 25
+            },
+                {
+                    "numar": "7",
+                    "frecventa_min": 15,
+                    "durata_calatorie_min": 22
+                }
+            ]
     },
-    {
-      "nume": "Stația Gara", 
-      "x": 90,
-      "y": 200, 
-      "linii": 
-      [{
-          "numar": "15",
-          "frecventa_min": 8,
-          "durata_calatorie_min": 30
-          }
-        ]
-      }, 
-    {
-      "nume": "Stația Universitate", 
-      "x": 150, 
-      "y": 140, 
-      "linii": 
-      [{
-          "numar": "5",
-          "frecventa_min": 12, 
-          "durata_calatorie_min": 20
-          }, 
         {
-          "numar": "11",
-          "frecventa_min": 20,
-          "durata_calatorie_min": 18
-        }] 
-      }, 
+            "nume": "Stația Gara",
+            "x": 90,
+            "y": 200,
+            "linii":
+                [{
+                    "numar": "15",
+                    "frecventa_min": 8,
+                    "durata_calatorie_min": 30
+                }
+                ]
+        },
+        {
+            "nume": "Stația Universitate",
+            "x": 150,
+            "y": 140,
+            "linii":
+                [{
+                    "numar": "5",
+                    "frecventa_min": 12,
+                    "durata_calatorie_min": 20
+                },
+                    {
+                        "numar": "11",
+                        "frecventa_min": 20,
+                        "durata_calatorie_min": 18
+                    }]
+        },
     ]
 
-  print("=== CĂUTARE STAȚIE OPTIMĂ ===\n")
-  print(f"Poziția ta: ({pozitie_student['x']}, {pozitie_student['y']})\n")
-  rezultate = gaseste_statie_optima(statii, pozitie_student) 
-  print(f"Analizate {len(statii)} stații:\n") 
+    print("=== CĂUTARE STAȚIE OPTIMĂ ===\n")
+    print(f"Poziția ta: ({pozitie_student['x']}, {pozitie_student['y']})\n")
+    rezultate = gaseste_statie_optima(statii, pozitie_student)
+    print(f"Analizate {len(statii)} stații:\n")
 
-  for i, analiza in enumerate(rezultate, 1):
+    for i, analiza in enumerate(rezultate, 1):
+        optiune = analiza['cea_mai_rapida_optiune']
+        print(f"{i}. {analiza['statie']}")
+        print(f"   Distanță: {analiza['distanta_la_statie_m']:.0f}m")
+        print(f"   Linie recomandată: {optiune['linie']}")
+        print(f"   └─ Mers pe jos: {optiune['timp_mers']:.1f} min")
+        print(f"   └─ Așteptare medie: {optiune['timp_asteptare']:.1f} min")
+        print(f"   └─ Călătorie: {optiune['timp_calatorie']} min")
+        print(f"   TOTAL: {optiune['timp_total']:.1f} minute\n")
 
-    optiune = analiza['cea_mai_rapida_optiune']
-    print(f"{i}. {analiza['statie']}")
-    print(f"   Distanță: {analiza['distanta_la_statie_m']:.0f}m")
-    print(f"   Linie recomandată: {optiune['linie']}")
-    print(f"   └─ Mers pe jos: {optiune['timp_mers']:.1f} min")
-    print(f"   └─ Așteptare medie: {optiune['timp_asteptare']:.1f} min")
-    print(f"   └─ Călătorie: {optiune['timp_calatorie']} min") 
-    print(f"   TOTAL: {optiune['timp_total']:.1f} minute\n") 
+        # Recomandare finală    
+    cea_mai_buna = rezultate[0]
+    print("=" * 50)
+    print("✓ RECOMANDARE:")
+    print(f"Mergi la {cea_mai_buna['statie']}")
+    print(f"Ia autobuzul {cea_mai_buna['cea_mai_rapida_optiune']['linie']}")
+    print(f"Timp total estimat: {cea_mai_buna['cea_mai_rapida_optiune']['timp_total']:.1f} minute")
 
-  # Recomandare finală     
-  cea_mai_buna = rezultate[0]
-  print("="*50) 
-  print("✓ RECOMANDARE:")
-  print(f"Mergi la {cea_mai_buna['statie']}")
-  print(f"Ia autobuzul {cea_mai_buna['cea_mai_rapida_optiune']['linie']}")
-  print(f"Timp total estimat: {cea_mai_buna['cea_mai_rapida_optiune']['timp_total']:.1f} minute")  
 
-if __name__ == "__main__":
-  main_autobuz()
+if name == "__main__":
+    main_autobuz()
