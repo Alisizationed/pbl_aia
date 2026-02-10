559b8b2f1c597a59ac3d784f80b8beab40739aab
Andreea Maevschi <octttopuss@gmail.com>
2026-02-10 11:46:07 +0200

    feat: quickSort

--- project 1/aia_sort.cpp
+++ project 1/aia_sort.cpp
@@ -67,7 +67,36 @@ void timSort(vector<int> &nums) {
 void sort_anastasia(vector<int> &nums);
 
 // TODO: Implement sorting algorithm
-void sort_andreea(vector<int> &nums);
+//Andreea
+
+int partition(vector<int> &nums, int low, int high) {
+    int pivot = nums[high];   // choose last element as pivot
+    int i = low - 1;
+
+    for (int j = low; j < high; j++) {
+        if (nums[j] <= pivot) {
+            i++;
+            swap(nums[i], nums[j]);
+        }
+    }
+
+    swap(nums[i + 1], nums[high]);
+    return i + 1;
+}
+
+void quickSort(vector<int> &nums, int low, int high) {
+    if (low < high) {
+        int pi = partition(nums, low, high);
+        quickSort(nums, low, pi - 1);
+        quickSort(nums, pi + 1, high);
+    }
+}
+
+void sortArray(vector<int> &nums) {
+    if (!nums.empty()) {
+        quickSort(nums, 0, nums.size() - 1);
+    }
+}
 
 // TODO: Implement sorting algorithm
 void sort_diana(vector<int> &nums);
