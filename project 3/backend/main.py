3392b6809a548c08700ef61878bb9f8eeec7d9f0
milenavg <milenavasilioglo@gmail.com>
2026-05-29 22:08:41 +0300

    feat: made train CRUD

--- project 3/backend/main.py
+++ project 3/backend/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from routers.optimize_router import router as optimize_router
+from routers.train_router import router as train_router
 
 app = FastAPI()
 
 app.include_router(optimize_router)
+app.include_router(train_router)
