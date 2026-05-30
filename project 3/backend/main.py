24feefafc388e157dff175892407a0b4ec8418db
milenavg <milenavasilioglo@gmail.com>
2026-05-30 14:38:05 +0300

    feat: done timewindiws CRUD

--- project 3/backend/main.py
+++ project 3/backend/main.py
@@ -4,6 +4,7 @@ from routers.train_router import router as train_router
 from routers.carriage_router import router as carriage_router
 from routers.node_router import router as node_router
 from routers.edge_router import router as edge_router
+from routers.time_window_router import router as time_window_router
 
 app = FastAPI()
 
@@ -12,3 +13,4 @@ app.include_router(train_router)
 app.include_router(carriage_router)
 app.include_router(node_router)
 app.include_router(edge_router)
+app.include_router(time_window_router)
