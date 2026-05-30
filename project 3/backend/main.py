2509a691fd10075eb93a1af0bbac725f3f1f0478
milenavg <milenavasilioglo@gmail.com>
2026-05-30 13:32:57 +0300

    feat: done Node CRUD

--- project 3/backend/main.py
+++ project 3/backend/main.py
@@ -2,9 +2,11 @@ from fastapi import FastAPI
 from routers.optimize_router import router as optimize_router
 from routers.train_router import router as train_router
 from routers.carriage_router import router as carriage_router
+from routers.node_router import router as node_router
 
 app = FastAPI()
 
 app.include_router(optimize_router)
 app.include_router(train_router)
 app.include_router(carriage_router)
+app.include_router(node_router)
