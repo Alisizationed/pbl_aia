ac9f4cc4271c2df795d481aa14e76f599e5ca7f7
milenavg <milenavasilioglo@gmail.com>
2026-05-31 20:19:15 +0300

    feat: made train CRUD (#43)

---
+++ project 3/backend/routers/train_router.py
@@ -0,0 +1,75 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+from database.database import SessionLocal
+from models.db_models import Train
+
+router = APIRouter(prefix="/trains", tags=["Trains"])
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@router.get("/")
+def get_trains(db: Session = Depends(get_db)):
+    return db.query(Train).all()
+
+
+@router.get("/{train_id}")
+def get_train(train_id: int, db: Session = Depends(get_db)):
+    train = db.query(Train).filter(Train.id == train_id).first()
+
+    if train is None:
+        raise HTTPException(status_code=404, detail="Train not found")
+
+    return train
+
+
+@router.post("/")
+def create_train(capacity: float, used_weight: float = 0, db: Session = Depends(get_db)):
+    train = Train(capacity=capacity, used_weight=used_weight)
+
+    db.add(train)
+    db.commit()
+    db.refresh(train)
+
+    return train
+
+
+@router.put("/{train_id}")
+def update_train(
+    train_id: int,
+    capacity: float,
+    used_weight: float,
+    db: Session = Depends(get_db)
+):
+    train = db.query(Train).filter(Train.id == train_id).first()
+
+    if train is None:
+        raise HTTPException(status_code=404, detail="Train not found")
+
+    train.capacity = capacity
+    train.used_weight = used_weight
+
+    db.commit()
+    db.refresh(train)
+
+    return train
+
+
+@router.delete("/{train_id}")
+def delete_train(train_id: int, db: Session = Depends(get_db)):
+    train = db.query(Train).filter(Train.id == train_id).first()
+
+    if train is None:
+        raise HTTPException(status_code=404, detail="Train not found")
+
+    db.delete(train)
+    db.commit()
+
+    return {"message": "Train deleted successfully"}
